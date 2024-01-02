a11d8c9728cf35bf7f597e9e14b9570fcc51561f
Sihun Lee <sstepoutside@gmail.com>
2024-01-02 22:14:29 +0900

    Fix: GRU structure, CNN activation

--- modules.py
+++ modules.py
@@ -30,11 +30,11 @@ class DilatedMaxPool1d(nn.Module):
         return self.maxpool(padded_x)
     
 class RNNModel(torch.nn.Module):
-    def __init__(self, input_size=84, hidden_size=128):
+    def __init__(self, input_size=84, hidden_size=128, num_layers=3):
         super().__init__()
-        self.gru = nn.GRU(input_size, hidden_size, batch_first=True, num_layers=3)
-        self.batch_norm = nn.BatchNorm1d(hidden_size)
-        self.proj = nn.Linear(hidden_size, 21)
+        self.gru = nn.GRU(input_size, hidden_size, batch_first=True, num_layers=num_layers, bidirectional=True)
+        self.batch_norm = nn.BatchNorm1d(hidden_size * 2)
+        self.proj = nn.Linear(hidden_size * 2, 21)
     
     def forward(self, x):
         x, _ = self.gru(x)
@@ -42,34 +42,43 @@ class RNNModel(torch.nn.Module):
         x = self.batch_norm(x)
         x = x.transpose(1, 2)
         x = self.proj(x)
-        return x
+        return x.sigmoid()
     
 class CNNModel(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.stack1 = nn.Sequential(
             nn.Conv2d(1, 128, (3, 3), (1, 1), "same", (1, 1)),
+            nn.LeakyReLU(0.2),
             nn.BatchNorm2d(128),
             nn.Conv2d(128, 64, (3, 3), (1, 1), "same", (1, 1)),
+            nn.LeakyReLU(0.2),
             nn.BatchNorm2d(64),
             DilatedMaxPool2d((3, 3), (1, 3), (1, 1)),
             nn.Conv2d(64, 128, (3, 3), (1, 1), "same", (3, 1)),
+            nn.LeakyReLU(0.2),
             nn.BatchNorm2d(128),
             nn.Conv2d(128, 64, (3, 3), (1, 1), "same", (3, 1)),
+            nn.LeakyReLU(0.2),
             nn.BatchNorm2d(64),
             DilatedMaxPool2d((3, 3), (1, 3), (3, 1)),
             nn.Conv2d(64, 128, (3, 3), (1, 1), "same", (9, 1)),
+            nn.LeakyReLU(0.2),
             nn.BatchNorm2d(128),
             nn.Conv2d(128, 64, (3, 3), (1, 1), "same", (9, 1)),
+            nn.LeakyReLU(0.2),
             nn.BatchNorm2d(64),
             DilatedMaxPool2d((3, 3), (1, 3), (9, 1)),
             nn.Conv2d(64, 64, (1, 4), (1, 1), 0, (1, 1)),
+            nn.LeakyReLU(0.2),
             nn.BatchNorm2d(64),
         )
         self.stack2 = nn.Sequential(
             nn.Conv1d(64, 128, 3, 1, "same", 27),
+            nn.LeakyReLU(0.2),
             nn.BatchNorm1d(128),
             nn.Conv1d(128, 64, 3, 1, "same", 27),
+            nn.LeakyReLU(0.2),
             nn.BatchNorm1d(64),
             DilatedMaxPool1d(3, 1, 27)
         )
