3705adb62abcf32c066e84fda6c0423e0f48670f
Sihun Lee <sstepoutside@gmail.com>
2024-03-09 01:42:52 +0900

    Fix: Change version split

--- constants.py
+++ constants.py
@@ -1,8 +1,8 @@
 # Dataset split
-TRAIN_VERSIONS = ['Bh', 'Bo', 'Fu', 'Kr', 'Ja', 'Th', 'Ke', 'Le']
-VALID_VERSIONS = ['Ha', 'So']
-VALID_VERSIONS = ['Ka']
+TRAIN_VERSIONS = ['Bh', 'Bo', 'Fu', 'Ja', 'Th', 'Ke', 'Le', 'Ka', 'Ne', 'Sa', 'We']
+VALID_VERSIONS = ['Sw', 'Kr']
+TEST_VERSIONS = []
 
 TRAIN_ACTS = ['A', 'B-1', 'B-2', 'C-1', 'C-2', 'D-0', 'D-1', 'D-2']
 VALID_ACTS = ['B-3', 'C-3']
-VALID_ACTS = ['D-3']
+TEST_ACTS = ['D-3']
