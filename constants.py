75e11c796b267e976d88fd67b85dab4447c6c7c1
Sihun Lee <sstepoutside@gmail.com>
2024-03-22 19:58:15 +0900

    Fix: Default config, match train versions with paper

--- constants.py
+++ constants.py
@@ -1,7 +1,8 @@
 # Dataset split
-TRAIN_VERSIONS = ['Bh', 'Bo', 'Kr', 'Sw', 'Th', 'Ke', 'Le', 'Ka', 'Ne', 'Sa', 'We']
-VALID_VERSIONS = ['Ja', 'Fu']
-TEST_VERSIONS = []
+TRAIN_VERSIONS = ['Bo', 'Bh', 'Fu', 'Ja', 'Ke', 'Kr', 'Le', 'Ne', 'Sw', 'Th']
+# VALID_VERSIONS = ['Ja', 'Fu']
+VALID_VERSIONS = ['Ka', 'Sw']
+TEST_VERSIONS = ['We']
 
 TRAIN_ACTS = ['A', 'B-1', 'B-2', 'B-3', 'C-1', 'C-2', 'D-0', 'D-1', 'D-2']
 VALID_ACTS = ['C-3', 'D-3']
