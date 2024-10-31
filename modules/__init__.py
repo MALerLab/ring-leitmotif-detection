161afa037d99c4a81b0cd4aa7b01cb05c164380d
issyun <sstepoutside@gmail.com>
2024-11-01 01:01:07 +0900

    Fix: Repo cleanup

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,5 +1,4 @@
 from .baselines import CNNModel, CRNNModel, CNNAttnModel
 from .film import FiLMModel, FiLMAttnModel
-from .bbox import BBoxModel
 from .yolo import YOLO, YOLOLoss
 from .yolo_utils import get_iou, nms, classwise_nms, get_acc, grid_to_absolute
